2044a2dcd041c214113f3edee42e41fa1c4bcc9b
shirosuke <tuketarou@gmail.com>
2020-05-02 16:05:32 +0900

    雛形配置

---
+++ spring/mrrs/src/main/java/mrrs/MrrsApplication.java
@@ -0,0 +1,13 @@
+package mrrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrrsApplication.class, args);
+	}
+
+}
---
+++ spring/mrrs/src/test/java/mrrs/MrrsApplicationTests.java
@@ -0,0 +1,13 @@
+package mrrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
